f3c9412774d6c679c4a9cdd1ec2f12e7dce7ca96
MarcoDiaz0 <bomabdou9@gmail.com>
2025-07-19 00:48:08 +0100

    1

--- Client/src/axios.ts
+++ Client/src/axios.ts
@@ -4,7 +4,7 @@ export const instance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : "/api",
+      : "/api/",
   headers: {
     "Content-Type": "application/json",
   },
