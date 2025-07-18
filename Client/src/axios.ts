e1329d9f5d351e0b9b6a9a6f866cb56641b2f0d7
MarcoDiaz0 <bomabdou9@gmail.com>
2025-07-19 00:19:53 +0100

    1

--- Client/src/axios.ts
+++ Client/src/axios.ts
@@ -1,10 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : "/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
   withCredentials: true, // Enable sending cookies with requests
 });
-
