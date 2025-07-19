1451e87238332aaf7b23b1e13409d8b80c990cf5
MarcoDiaz0 <bomabdou9@gmail.com>
2025-07-19 07:40:26 +0100

    1

--- Server/Index.js
+++ Server/Index.js
@@ -29,7 +29,7 @@ app.use("/api/auth", AuthRouter);
 app.use("/api/messages", MessageRouter);
 
 if (process.env.NODE_ENV === "production") {
- app.use(express.static(path.join(__dirname, "../Client/dist")));
+ app.use(e.static(path.join(__dirname, "../Client/dist")));
 
  app.get("*", (req, res) => {
    res.sendFile(path.join(__dirname, "../Client/dist/index.html"));
